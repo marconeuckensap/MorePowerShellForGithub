ac2b9f5904255a2ea564814fc5151b4908562b87
marconeuckensap <marco.neuckens@student.ap.be>
2021-06-04 22:01:46 +0200

    powershell script that gives the token and makes name and location on your git hub also creates a new repository

---
+++ Token_001.ps1
@@ -0,0 +1,28 @@
+﻿
+$Token = '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'
+$Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
+
+$headers = 
+@{
+    Authorization = 'Basic {0}' -f $Base64Token;
+};
+
+
+$Body = 
+@{
+    name = 'Marco Neuckens';
+    location = 'Hollow Knight';
+} | ConvertTo-Json
+
+
+Invoke-RestMethod -Headers $headers -Uri https://api.github.com/user -body $Body -Method Patch
+
+
+$repo = 
+@{
+    repo = New-GitHubRepository -RepositoryName test_repo_001;
+    description = 'An other repository created in powershell';
+
+} | ConvertTo-Json
+
+Invoke-RestMethod -Headers $headers -Uri https://api.github.com/user -body $repo -Method Patch
---
+++ weergeven-repos.ps1
@@ -0,0 +1,9 @@
+﻿$Token = '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'
+$Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
+
+for (($i=1), ($repos=@()), ($res=$null); ($i -eq 1) -or ($res.Count); $i++) {
+Write-Host $i
+$res = Invoke-RestMethod -Headers $headers -Uri https://api.github.com/user/repos?page=$i
+$repos += $res
+}
+$repos.ssh_url
