1428b6f493f3a2a9fa047e884f5096d37b3adf3c
marconeuckensap <marco.neuckens@student.ap.be>
2021-06-05 15:42:29 +0200

    denkoeffening with explenation of the script

---
+++ denkoeffening.ps1
@@ -0,0 +1,6 @@
+﻿# maakt een nieuwe repo van de huidige dir. get-item .name zal gebruiker maken van de naam van huidige dir
+$repo = New-GitHubRepository -Name (Get-Item .).Name
+# maakt een private repo aan 
+$repo | Set-GitHubRepository -Private
+#opvragen van de repo.
+$repo = ($repo | Get-GitHubRepository)
