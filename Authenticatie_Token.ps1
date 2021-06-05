35819a16b1ffb7a3de3c4f26a4ae5a7d4922674e
marconeuckensap <marco.neuckens@student.ap.be>
2021-06-05 13:27:04 +0200

    get auth and get history is added

--- Authenticatie_Token.ps1
+++ Authenticatie_Token.ps1
@@ -1,6 +1,7 @@
 ﻿Read-Host -AsSecureString -Prompt ’token’ |
 # converts string to secure data
 ConvertFrom-SecureString |
+# Tee-Object writes the outcome to a  given file name 
 Tee-Object .\secret.txt |
 # converting token to a secure string
 ConvertTo-SecureString |
@@ -10,3 +11,7 @@ Set-Variable ss_token
 $creds = New-Object pscredential 'user’, $ss_token
 Set-GitHubAuthentication -SessionOnly `
 -Credential $creds
+# Testing to see if you are logged on.
+Test-GitHubAuthenticationConfigured
+
+Get-History | Tee-Object .\snippets.ps1
