7ab75e14c4dbc7cddff1b9d03ae1759150531da6
marconeuckensap <marco.neuckens@student.ap.be>
2021-06-05 14:08:40 +0200

    script that create a repo and puts it on github

---
+++ RepoMakenPS.ps1
@@ -0,0 +1,18 @@
+﻿$Token = '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'
+# converting for token
+$Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
+
+$headers = 
+@{
+    Authorization = 'Basic {0}' -f $Base64Token;
+};
+
+#creating new nepo named test.
+$repo = 
+@{
+    repo = New-GitHubRepository -RepositoryName test;
+    description = 'An other repository created in powershell';
+
+} | ConvertTo-Json
+#new repo to github
+Invoke-RestMethod -Headers $headers -Uri https://api.github.com/user -body $repo -Method Patch
