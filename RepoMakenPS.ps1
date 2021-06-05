c999faa0989aac10d00986b10ba1afb56167e6ba
marconeuckensap <marco.neuckens@student.ap.be>
2021-06-05 14:37:22 +0200

    script that helps you make adjustments to your profile

--- RepoMakenPS.ps1
+++ RepoMakenPS.ps1
@@ -1,18 +1,24 @@
-﻿$Token = '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'
-# converting for token
-$Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
+﻿$ss_token = Read-Host -AsSecureString -Prompt ’token’ |
+ConvertFrom-SecureString |
+Tee-Object ./secret.txt |
+ConvertTo-SecureString
+$creds = New-Object pscredential 'user’, $ss_token
+Set-GitHubAuthentication -SessionOnly `
+-Credential $creds
+
+$Base64Token = [System.Convert]::ToBase64String([char[]]$creds.GetNetworkCredential().Password)
+
+$api = "https://api.github.com/user"
 
 $headers = 
 @{
     Authorization = 'Basic {0}' -f $Base64Token;
 };
-
-#creating new nepo named test.
 $repo = 
 @{
-    repo = New-GitHubRepository -RepositoryName test;
+    repo = New-GitHubRepository -RepositoryName test_repo;
     description = 'An other repository created in powershell';
 
 } | ConvertTo-Json
-#new repo to github
-Invoke-RestMethod -Headers $headers -Uri https://api.github.com/user -body $repo -Method Patch
+
+Invoke-RestMethod -Headers $headers -Uri $api -body $repo -Method Patch
