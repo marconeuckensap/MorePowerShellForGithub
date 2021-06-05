cb539ec9130ff14b0d7bc4b99f20a42345de553a
marconeuckensap <marco.neuckens@student.ap.be>
2021-06-05 12:31:57 +0200

    script that converts the token

---
+++ Authenticatie_Token.ps1
@@ -0,0 +1,12 @@
+﻿Read-Host -AsSecureString -Prompt ’token’ |
+# converts string to secure data
+ConvertFrom-SecureString |
+Tee-Object .\secret.txt |
+# converting token to a secure string
+ConvertTo-SecureString |
+# sets variable for security.securestring
+Set-Variable ss_token
+#user and token set to variable creds
+$creds = New-Object pscredential 'user’, $ss_token
+Set-GitHubAuthentication -SessionOnly `
+-Credential $creds
