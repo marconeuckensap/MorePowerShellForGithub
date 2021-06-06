8c2048d4890b1718e1d6b4636727e26057349ea3
marconeuckensap <marco.neuckens@student.ap.be>
2021-06-06 12:43:50 +0200

    creating function get-authHeader

---
+++ more_powerschell_for_github(get-authheader).ps1
@@ -0,0 +1,28 @@
+function get-AuthHeader
+{
+    $C = Get-Credential
+
+    $ss_token = Read-Host -AsSecureString -Prompt ’token’ |
+    ConvertFrom-SecureString |
+    Tee-Object ./secret.txt |
+    ConvertTo-SecureString
+    $creds = New-Object pscredential 'user’, $ss_token
+    Set-GitHubAuthentication -SessionOnly `
+    -Credential $creds
+
+    $Base64Token = [System.Convert]::ToBase64String([char[]]$creds.GetNetworkCredential().Password)
+
+    $api = "https://api.github.com/user"
+
+    $headers = 
+    @{
+        Authorization = 'Basic {0}' -f $Base64Token;
+
+        
+    };  
+  
+    Invoke-RestMethod -Headers $headers -Uri $api
+    
+}
+
+
