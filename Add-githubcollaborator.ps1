88edfb1929aceaf5fe2c7323f8edd2d88ebbc265
marconeuckensap <marco.neuckens@student.ap.be>
2021-06-07 19:34:58 +0200

    not compleetly rdy dont work yet

---
+++ Add-githubcollaborator.ps1
@@ -0,0 +1,20 @@
+﻿function Add-GitHubCollaborator {
+
+param ($Credential, $Repository, $Collaborator)
+
+Get-AuthHeader -Credential $Credential
+
+$global:api = "https://api.github.com/repos/$Credential/$Repository/collaborators/$Collaborator"
+
+$Body = 
+@{
+    username = $Collaborator;
+    repo = $Repository;
+    owner = $Credential;
+} | ConvertTo-Json
+
+Invoke-RestMethod -Headers $headers -Uri $api -body $Body -Method Put  
+}
+<#
+na het inloggen loop ik tegen een error aan die ik momenteel niet opgelost krijg
+#>
